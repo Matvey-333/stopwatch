be9457bbca01b2705068719e674e9b3b8afc94ed
Matvey <babicm049@gmail.com>
2024-10-12 21:21:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import time
 
-# Запрашиваем у пользователя количество минут и секунд
+
 minutes = int(input("Введите количество минут: "))
 seconds = int(input("Введите количество секунд: "))
 
-# Преобразуем все время в секунды
+
 total_seconds = minutes * 60 + seconds
 
-# Запускаем цикл обратного отсчета
 while total_seconds > 0:
-    mins, secs = divmod(total_seconds, 60)  # Получаем минуты и секунды
+    mins, secs = divmod(total_seconds, 60)
     print(f"Осталось: {mins} минут {secs} секунд")
     time.sleep(1)  # Ожидание 1 секунду
-    total_seconds -= 1
-
-# Сообщение, когда время вышло
+    total_seconds -= 
 print("Время вышло!")
